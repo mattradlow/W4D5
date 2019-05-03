9d2c345d5a5aecfa0113249b8eed9cc87d6bcdb3
App Academy Student <aa@Apps-Mac-mini-2.local>
2019-05-03 12:13:26 -0400

    Setup testing enviornment

--- achievement_app/test/test_helper.rb
+++ achievement_app/test/test_helper.rb
@@ -7,4 +7,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
